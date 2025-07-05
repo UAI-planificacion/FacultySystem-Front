d6cddd0851c39cf484a680d78f9f0bf41a7efb6c
kevinkeyssx <keyss1700@gmail.com>
2025-07-04 22:10:52 -0400

    Interfaces tabs added

--- app/types/index.ts
+++ app/types/index.ts
@@ -3,33 +3,38 @@
 export type Role = 'admin' | 'editor' | 'viewer';
 
 export interface Person {
-  id: string;
-  name: string;
-  email: string;
-  position: string;
-  role: Role;
+    id          : string;
+    name        : string;
+    email       : string;
+    position    : string;
+    role        : Role;
 }
 
 export interface CostCenter {
-  id: string;
-  code: string;
-  name: string;
+    id      : string;
+    code    : string;
+    name    : string;
 }
 
 export interface Subject {
-  id: string;
-  name: string;
-  code: string;
-  maxStudents: number;
-  description?: string;
-  costCenter?: CostCenter;
+    id              : string;
+    name            : string;
+    code            : string;
+    maxStudents     : number;
+    description?    : string;
+    costCenter?     : CostCenter;
+}
+
+export interface Request {
+    id : string;
+    name : string;
 }
 
 export interface Faculty {
-  id: string;
-  name: string;
-  code: string;
-  description?: string;
-  subjects: Subject[];
-  personnel: Person[];
+    id              : string;
+    name            : string;
+    description?    : string;
+    subjects        : Subject[];
+    personnel       : Person[];
+    requests        : Request[];
 }
