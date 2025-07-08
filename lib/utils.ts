89159da5dec60a239366221f1936b0389f36a6ba
kevinkeyssx <keyss1700@gmail.com>
2025-07-07 21:50:32 -0400

    dateToString implemented

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,27 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { parseISO } from 'date-fns';
+
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
+}
+
+
+export function dateToString( dateInput?: Date | string | undefined ): string {
+    if ( !dateInput ) return "-";
+
+    const date = typeof dateInput === 'string'
+        ? parseISO( dateInput )
+        : dateInput;
+
+    if ( isNaN( date.getTime() )) return "-";
+
+    const opciones: Intl.DateTimeFormatOptions = {
+        day     : '2-digit',
+        month   : '2-digit',
+        year    : 'numeric'
+    };
+
+    return new Intl.DateTimeFormat( 'es-ES', opciones ).format( date );
 }
