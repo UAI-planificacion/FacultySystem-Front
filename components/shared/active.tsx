8e124935c130e68087d6fba02799ba3cbb3f39e6
kevinkeyssx <keyss1700@gmail.com>
2025-07-08 20:04:38 -0400

    Active component added

---
+++ components/shared/active.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { CircleCheckBig, CircleX } from "lucide-react";
+import { Badge } from "@/components/ui/badge"
+
+export function ActiveBadge(
+    {
+        isActive
+    }: {
+        isActive: boolean;
+    }
+) {
+    return (
+        <div className="flex items-center gap-2">
+            {isActive ? <CircleCheckBig className="w-4 h-4 text-green-600" /> : <CircleX className="w-4 h-4 text-red-600" />}
+
+            <Badge
+                variant={isActive ? 'default' : 'destructive'}
+                className={`${isActive ? 'bg-green-600' : 'bg-red-600'} text-white`}
+            >
+                {isActive ? 'Activo' : 'Inactivo'}
+            </Badge>
+        </div>
+    )
+}
