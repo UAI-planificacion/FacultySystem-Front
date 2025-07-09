8c4c89c5c3ba6d51b1e24f340b02529dda58d74a
kevinkeyssx <keyss1700@gmail.com>
2025-07-08 20:06:56 -0400

    Request type updated

--- types/request.ts
+++ types/request.ts
@@ -1,38 +1,40 @@
+import { Role } from "./staff.model";
+
 export enum Status {
-    PENDING = "PENDING",
-    APPROVED = "APPROVED",
-    REJECTED = "REJECTED",
-    REVIEWING = "REVIEWING",
+    PENDING     = "PENDING",
+    APPROVED    = "APPROVED",
+    REJECTED    = "REJECTED",
+    REVIEWING   = "REVIEWING",
 }
 
 
 export enum SpaceType {
-    ROOM = "ROOM",
-    AUDITORIUM = "AUDITORIUM",
-    COMMUNIC = "COMMUNIC",
-    LAB = "LAB",
-    LABPC = "LABPC",
-    DIS = "DIS",
-    GARAGE = "GARAGE",
-    CORE = "CORE",
+    ROOM        = "ROOM",
+    AUDITORIUM  = "AUDITORIUM",
+    COMMUNIC    = "COMMUNIC",
+    LAB         = "LAB",
+    LABPC       = "LABPC",
+    DIS         = "DIS",
+    GARAGE      = "GARAGE",
+    CORE        = "CORE",
 }
 
 export enum Size {
-    XS = "XS",
-    XE = "XE",
-    S = "S",
-    SE = "SE",
-    MS = "MS",
-    M = "M",
-    L = "L",
-    XL = "XL",
+    XS  = "XS",
+    XE  = "XE",
+    S   = "S",
+    SE  = "SE",
+    MS  = "MS",
+    M   = "M",
+    L   = "L",
+    XL  = "XL",
     XXL = "XXL",
 }
 
 export enum Nivel {
-    PREGRADO = "PREGRADO",
-    FIRST_GRADE = "FIRST_GRADE",
-    SECOND_GRADE = "SECOND_GRADE",
+    PREGRADO        = "PREGRADO",
+    FIRST_GRADE     = "FIRST_GRADE",
+    SECOND_GRADE    = "SECOND_GRADE",
 }
 
 export enum Building {
@@ -44,64 +46,60 @@ export enum Building {
     F = "F",
 }
 
-export interface Staff {
-    id: string
-    name: string
-    email: string
+export interface StaffRequest {
+    id      : string;
+    name    : string;
+    email   : string;
+    role    : Role;
 }
 
-export interface Subject {
-    id: string
-    name: string
-    code: string
+export interface SubjectRequest {
+    id      : string;
+    name    : string;
 }
 
 export interface Professor {
-    id: string
-    name: string
-    email: string
+    id      : string;
+    name    : string;
+    email   : string;
 }
 
 export interface RequestDetail {
-    id: string
-    minimum?: number
-    maximum?: number
-    spaceType?: SpaceType
-    spaceSize?: Size
-    costCenterId?: string
-    inAfternoon: boolean
-    building?: Building
-    description?: string
-    comment?: string | null
-    moduleId?: string
-    days: string[]
-    spaceId?: string
-    isPriority: boolean
-    nivel: Nivel
-    professorId?: string
-    professor?: Professor
-    staffCreateId?: string | null
-    staffCreate?: Staff | null
-    staffUpdateId?: string | null
-    staffUpdate?: Staff | null
-    requestId: string
-    createdAt: Date
-    updatedAt: Date
+    id              : string;
+    requestId       : string;
+    minimum         : number | null;
+    maximum         : number | null;
+    spaceType       : SpaceType | null;
+    spaceSize       : Size | null;
+    costCenterId    : string | null;
+    inAfternoon     : boolean;
+    building        : Building | null;
+    description     : string | null;
+    comment         : string | null;
+    moduleId        : string | null;
+    days            : string[];
+    spaceId         : string | null;
+    isPriority      : boolean;
+    nivel           : Nivel;
+    professor       : Professor | null;
+    staffCreate     : StaffRequest;
+    staffUpdate     : StaffRequest | null;
+    createdAt       : Date;
+    updatedAt       : Date;
 }
 
+
 export interface Request {
-    id: string
-    status: Status
-    isConsecutive: boolean
-    description?: string
-    comment?: string | null
-    staffCreateId: string
-    staffCreate: Staff
-    staffUpdateId?: string | null
-    staffUpdate?: Staff | null
-    subjectId: string
-    subject: Subject
-    details: RequestDetail[]
-    createdAt: Date
-    updatedAt: Date
+    id              : string;
+    title           : string;
+    status          : Status;
+    isConsecutive   : boolean;
+    description     : string | null;
+    comment         : string | null;
+    updatedAt       : Date;
+    createdAt       : Date;
+    staffCreate     : StaffRequest;
+    staffUpdate     : StaffRequest | null;
+    subject         : SubjectRequest;
+    totalDetails    : number;
 }
