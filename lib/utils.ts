98be3af9c6317a86197449f64e11a46de6ee88f8
kevinkeyssx <keyss1700@gmail.com>
2025-07-09 20:07:06 -0400

    tempoFormat added

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,9 @@ import { twMerge } from 'tailwind-merge';
 
 import { Status } from '@/types/request';
 
+import { format, parse } from "@formkit/tempo"
+
+
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -13,6 +16,13 @@ export function dateToString( dateInput?: Date | string | undefined ): string {
     if ( !dateInput ) return "-";
 
     return dateInput instanceof Date ? dateInput.toISOString().split('T')[0] : dateInput;
+}
+
+
+export function tempoFormat( dateInput: Date | string | undefined ) {
+    if ( !dateInput ) return "-";
+
+    return format( dateInput, "D MMMM YYYY" );
 }
 
 export const getStatusColor = ( status: Status ): string => ({
