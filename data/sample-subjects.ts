6b77bb6de5c465024ae561845fd54064604eb01b
kevinkeyssx <keyss1700@gmail.com>
2025-07-09 20:06:47 -0400

    Subject Data added

---
+++ data/sample-subjects.ts
@@ -0,0 +1,14 @@
+export interface SubjectOption {
+    id: string;
+    name: string;
+    code: string;
+}
+
+export const sampleSubjects: SubjectOption[] = [
+    { id: '1', name: 'Matemáticas Avanzadas', code: 'MATH-101' },
+    { id: '2', name: 'Programación I', code: 'PROG-201' },
+    { id: '3', name: 'Base de Datos', code: 'DB-301' },
+    { id: '4', name: 'Inteligencia Artificial', code: 'AI-401' },
+    { id: '5', name: 'Redes de Computadoras', code: 'NET-501' },
+    { id: '6', name: 'Seguridad Informática', code: 'SEC-601' },
+];
