7670b1ba0dc6707ad9ca4dd3c29784aecc9f2ee9
kevinkeyssx <keyss1700@gmail.com>
2025-07-09 20:03:44 -0400

    Comment removed & fetchApi implemented

--- components/request/request.tsx
+++ components/request/request.tsx
@@ -1,34 +1,30 @@
 "use client"
 
 import { useState } from "react"
-import RequestList from "./request-list"
-import RequestDetailView from "../request-detail/request-detail"
-import { type Request, Status, SpaceType, Size, Nivel, Building } from "@/types/request"
+
 import { useQuery } from "@tanstack/react-query"
-import { KEY_QUERYS } from "@/consts/key-queries"
-import { fetchData } from "@/services/fetch"
+
+import { RequestList }          from "@/components/request/request-list"
+import { RequestDetailView }    from "@/components/request-detail/request-detail"
+
+import { KEY_QUERYS }   from "@/consts/key-queries";
+import { type Request } from "@/types/request";
+import { fetchApi }     from "@/services/fetch";
+
 
 interface RequestsManagementProps {
     facultyId: string;
     enabled: boolean;
 }
 
-export default function RequestsManagement({ facultyId, enabled }: RequestsManagementProps) {
 
-    const {
-        data,
-        isLoading,
-        error,
-        isError,
-        refetch
-    } = useQuery({
+export function RequestsManagement({ facultyId, enabled }: RequestsManagementProps) {
+
+    const { data, isLoading, isError } = useQuery({
         queryKey    : [ KEY_QUERYS.REQUESTS, facultyId ],
-        queryFn     : () => fetchData<Request[]>( `requests/faculty/${facultyId}` ),
+        queryFn     : () => fetchApi<Request[]>( `requests/faculty/${facultyId}` ),
         enabled,
     });
-
-
-    // const [requests, setRequests] = useState<Request[]>(mockRequests)
     const [selectedRequest, setSelectedRequest] = useState<Request | null>(null)
 
     const handleViewDetails = (request: Request) => {
@@ -39,10 +35,6 @@ export default function RequestsManagement({ facultyId, enabled }: RequestsManag
         setSelectedRequest(null)
     }
 
-    const handleUpdateRequest = (updatedRequest: Request) => {
-        // setRequests((prev) => prev.map((req) => (req.id === updatedRequest.id ? updatedRequest : req)))
-        setSelectedRequest(updatedRequest)
-    }
 
     return (
             isLoading 
@@ -51,7 +43,6 @@ export default function RequestsManagement({ facultyId, enabled }: RequestsManag
                 <RequestDetailView
                     request  = { selectedRequest }
                     onBack      = { handleBack }
-                    // onUpdateRequest = { handleUpdateRequest }
                 />
             ) : (
                 <RequestList
@@ -59,11 +50,5 @@ export default function RequestsManagement({ facultyId, enabled }: RequestsManag
                     onViewDetails   = { handleViewDetails }
                 />
             )
-        // <div className="container mx-auto p-6">
-            /* <div className="mb-6">
-                <h1 className="text-3xl font-bold">Solicitudes de Facultad</h1>
-                <p className="text-muted-foreground">Gestiona las solicitudes de espacios y recursos de la facultad</p>
-            </div> */
-        // </div>
     )
 }
