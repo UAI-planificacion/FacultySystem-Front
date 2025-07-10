4f30c9b980ff71c075defe9ef43a6a8416f27360
kevinkeyssx <keyss1700@gmail.com>
2025-07-09 20:07:37 -0400

    Subject model updated

--- types/subject.model.ts
+++ types/subject.model.ts
@@ -1,13 +1,24 @@
-
-export interface Subject {
+interface BaseSubject {
     id              : string;
     name            : string;
     startDate?      : Date;
     endDate?        : Date;
     students        : number;
     costCenterId    : string;
-    isActive        : boolean;
+}
+
+
+export interface Subject extends BaseSubject {
     facultyId       : string;
+    isActive        : boolean;
     createdAt       : Date;
     updatedAt       : Date;
 }
+
+
+export interface CreateSubject extends BaseSubject {
+    facultyId       : string;
+}
+
+
+export interface UpdateSubject extends Partial<BaseSubject> {}
