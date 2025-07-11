281a15284197d4400f1d34e2ce6d0ab4a6900270
kevinkeyssx <keyss1700@gmail.com>
2025-07-10 20:38:09 -0400

    DeleteConfirmDialog implemented & updated service done

--- components/request/request-list.tsx
+++ components/request/request-list.tsx
@@ -2,45 +2,103 @@
 
 import { useState, useMemo, useEffect } from "react";
 
+import {
+    useMutation,
+    useQueryClient
+}                   from "@tanstack/react-query";
+import { toast }    from "sonner";
+
+import {
+    RequestForm,
+    RequestFormValues
+}                               from "@/components/request/request-form";
 import { Card, CardContent }    from "@/components/ui/card";
 import { RequestFilter }        from "@/components/request/request-filter";
 import { RequestCard }          from "@/components/request/request-card";
-import { RequestForm }          from "@/components/request/request-form";
+import { DeleteConfirmDialog }  from "@/components/dialog/DeleteConfirmDialog";
 
-import { type Request, Status } from "@/types/request";
+import { type Request, Status, UpdateRequest }  from "@/types/request";
+import { Method, fetchApi }                     from "@/services/fetch";
+import { errorToast, successToast }             from "@/config/toast/toast.config";
+import { KEY_QUERYS }                           from "@/consts/key-queries";
 
 
 interface RequestListProps {
-    requests: Request[]
-    onViewDetails: (request: Request) => void
+    requests        : Request[];
+    onViewDetails   : ( request: Request ) => void;
 }
 
-export function RequestList({ requests, onViewDetails }: RequestListProps) {
-    const [isOpen, setIsOpen] = useState(false)
-    const [editinRequest, setEditinRequest] = useState<Request>( {id: 'test', subject: {id: 'test', name: 'test'}} as Request );
-    const [searchId, setSearchId] = useState("")
-    const [statusFilter, setStatusFilter] = useState<Status | "ALL">("ALL")
-    const [consecutiveFilter, setConsecutiveFilter] = useState<"ALL" | "TRUE" | "FALSE">("ALL")
-    const [sortBy, setSortBy] = useState<"status" | "staffCreate" | "staffUpdate" | "subjectId" | "createdAt">(
-        "createdAt",
-    )
-    const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
+
+type SortBy             = "status" | "staffCreate" | "staffUpdate" | "subjectId" | "createdAt";
+type SortOrder          = "asc" | "desc";
+type ConsecutiveFilter  = "ALL" | "TRUE" | "FALSE";
+
+
+const startRequest = {id: 'test', subject: {id: 'test', name: 'test'}} as Request;
+
+
+export function RequestList({
+    requests,
+    onViewDetails
+}: RequestListProps ): JSX.Element {
+    const queryClient                               = useQueryClient();
+    const [isOpen, setIsOpen]                       = useState( false );
+    const [selectedRequest, setSelectedRequest]     = useState<Request>( startRequest );
+    const [title, setTitle]                         = useState( "" );
+    const [statusFilter, setStatusFilter]           = useState<Status | "ALL">( "ALL" );
+    const [consecutiveFilter, setConsecutiveFilter] = useState<ConsecutiveFilter>( "ALL" );
+    const [sortBy, setSortBy]                       = useState<SortBy>( "createdAt" );
+    const [sortOrder, setSortOrder]                 = useState<SortOrder>( "desc" );
+    const [isDeleteDialogOpen, setIsDeleteDialogOpen]   = useState( false );
+
 
     useEffect(() => {
-        setEditinRequest( requests[0] );
+        setSelectedRequest( requests[0] );
     }, [requests]);
 
+
+    const updateRequestApi = async ( updatedRequest: UpdateRequest ): Promise<Request>  =>
+        fetchApi<Request>( `requests/${updatedRequest.id}`, Method.PATCH, updatedRequest );
+
+
+    const deleteRequestApi = async ( requestId: string ): Promise<Request> =>
+        fetchApi<Request>( `requests/${requestId}`, Method.DELETE );
+
+
+    const updateRequestMutation = useMutation<Request, Error, UpdateRequest>({
+        mutationFn: updateRequestApi,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: [KEY_QUERYS.REQUESTS] });
+            setIsOpen( false );
+            setSelectedRequest( startRequest );
+            toast( 'Solicitud actualizada exitosamente', successToast );
+        },
+        onError: ( mutationError ) => toast( `Error al actualizar la solicitud: ${mutationError.message}`, errorToast )
+    });
+
+
+    const deleteRequestMutation = useMutation<Request, Error, string>({
+        mutationFn: deleteRequestApi,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: [KEY_QUERYS.REQUESTS] });
+            setIsDeleteDialogOpen( false );
+            toast( 'Solicitud eliminada exitosamente', successToast );
+        },
+        onError: ( mutationError ) => toast( `Error al eliminar solicitud: ${mutationError.message}`, errorToast )
+    });
+
+
     const filteredAndSortedRequests = useMemo(() => {
-        const filtered = requests.filter((request) => {
-            const matchesId = searchId === "" || request.id.toLowerCase().includes(searchId.toLowerCase())
-            const matchesStatus = statusFilter === "ALL" || request.status === statusFilter
+        const filtered = requests.filter( request => {
+            const matchesId = title === "" || request.title.toLowerCase().includes( title.toLowerCase() );
+            const matchesStatus = statusFilter === "ALL" || request.status === statusFilter;
             const matchesConsecutive =
                 consecutiveFilter === "ALL" ||
-                (consecutiveFilter === "TRUE" && request.isConsecutive) ||
-                (consecutiveFilter === "FALSE" && !request.isConsecutive)
+                ( consecutiveFilter === "TRUE" && request.isConsecutive ) ||
+                ( consecutiveFilter === "FALSE" && !request.isConsecutive );
 
-            return matchesId && matchesStatus && matchesConsecutive
-        })
+            return matchesId && matchesStatus && matchesConsecutive;
+        });
 
         return filtered.sort(( a, b ) => {
             const [aValue, bValue] = {
@@ -56,25 +114,35 @@ export function RequestList({ requests, onViewDetails }: RequestListProps) {
 
             return 0;
         })
-    }, [requests, searchId, statusFilter, consecutiveFilter, sortBy, sortOrder]);
+    }, [requests, title, statusFilter, consecutiveFilter, sortBy, sortOrder]);
 
 
-    function onEdit( request: Request ) {
-        setEditinRequest( request );
+    const handleFormSubmit = ( formData: RequestFormValues ): void => {
+        updateRequestMutation.mutate({
+            ...formData,
+            id: selectedRequest.id
+        });
+    };
+
+
+    function onEdit( request: Request ): void {
+        setSelectedRequest( request );
         setIsOpen(true );
     }
 
-    // function onDelete( request: Request ) {
-    //     setDeletingStaffId( request.id );
-    //     setIsDeleteDialogOpen(true );
-    // }
+
+    function openDeleteDialog( id: string ): void {
+        setSelectedRequest( requests.find(request => request.id === id) || startRequest );
+        setIsDeleteDialogOpen( true );
+    }
+
 
     return (
         <div className="space-y-4">
             {/* Filters */}
             <RequestFilter
-                searchId                = { searchId }
-                setSearchId             = { setSearchId }
+                title                   = { title }
+                setTitle                = { setTitle }
                 statusFilter            = { statusFilter }
                 setStatusFilter         = { setStatusFilter }
                 consecutiveFilter       = { consecutiveFilter }
@@ -87,13 +155,13 @@ export function RequestList({ requests, onViewDetails }: RequestListProps) {
 
             {/* Results */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {filteredAndSortedRequests.map((request) => (
+                {filteredAndSortedRequests.map( request => (
                     <RequestCard
                         request         = { request }
                         key             = { request.id }
                         onViewDetails   = { () => onViewDetails( request )}
                         onEdit          = { () => onEdit( request )}
-                        onDelete        = { () => {}}
+                        onDelete        = { () => openDeleteDialog( request.id )}
                     />
                 ))}
             </div>
@@ -108,9 +176,18 @@ export function RequestList({ requests, onViewDetails }: RequestListProps) {
 
             <RequestForm
                 isOpen      = { isOpen }
-                onClose     = { () => setIsOpen(false) }
-                onSubmit    = { () =>{} }
-                data        = { editinRequest }
+                onClose     = { () => setIsOpen( false )}
+                onSubmit    = { handleFormSubmit }
+                data        = { selectedRequest }
+            />
+
+            {/* Delete Confirmation Dialog */}
+            <DeleteConfirmDialog
+                isOpen      = { isDeleteDialogOpen }
+                onClose     = { () => setIsDeleteDialogOpen( false )}
+                onConfirm   = { () => deleteRequestMutation.mutate( selectedRequest.id || '') }
+                name        = { selectedRequest.title || '' }
+                type        = "la Solicitud (y todos sus detalles relacionados)"
             />
         </div>
     );
