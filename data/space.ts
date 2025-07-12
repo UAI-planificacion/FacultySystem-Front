dcf93b7451e5409f515523134d220f1371388189
kevinkeyssx <keyss1700@gmail.com>
2025-07-11 20:14:58 -0400

    Spaces mock data added

---
+++ data/space.ts
@@ -0,0 +1,17 @@
+interface SpaceMock {
+    id: string;
+    name: string;
+  }
+  
+  export const spacesMock: SpaceMock[] = [
+    { id: "S001", name: "Aula Magna A" },
+    { id: "S002", name: "Laboratorio de Ciencias Exactas" },
+    { id: "S003", name: "Auditorio Principal" },
+    { id: "S004", name: "Sala de Seminarios 101" },
+    { id: "S005", name: "Oficina de Proyectos de Ingeniería" },
+    { id: "S006", name: "Gimnasio Cubierto" },
+    { id: "S007", name: "Sala de Reuniones B2" },
+    { id: "S008", name: "Cubiculo de Investigación 3" },
+    { id: "S009", name: "Estudio de Grabación Audiovisual" },
+    { id: "S010", name: "Sala de Prácticas de Enfermería" },
+  ];
