71a123d2415937747add67cabc2461ee2f10ad5d
kevinkeyssx <keyss1700@gmail.com>
2025-07-14 21:13:33 -0400

    KEY_QUERYS updated

--- consts/key-queries.ts
+++ consts/key-queries.ts
@@ -3,5 +3,13 @@ export const KEY_QUERYS = {
     SUBJECTS        : 'subjects',
     STAFF           : 'staff',
     REQUESTS        : 'requests',
-    REQUEST_DETAIL  : 'request-detail'
+    REQUEST_DETAIL  : 'request-detail',
+
+    PROFESSORS      : 'professors',
+    SIZE            : 'sizes',
+    DAYS            : 'days',
+    SPACES          : 'spaces',
+    MODULES         : 'modules',
+
+    COST_CENTERS    : 'cost-centers',
 }
