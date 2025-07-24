ab87f1539117f70982c07d80cf1d1aaf56ec170b
kevinkeyssx <keyss1700@gmail.com>
2025-07-23 22:23:08 -0400

    Styles updated

--- components/shared/consecutive.tsx
+++ components/shared/consecutive.tsx
@@ -1,7 +1,12 @@
 'use client'
 
-import { GripHorizontal } from "lucide-react"
-import { Badge } from "../ui/badge"
+import { JSX } from "react";
+
+import { GripHorizontal } from "lucide-react";
+
+import { Badge }    from "@/components/ui/badge";
+import { cn }       from "@/lib/utils";
+
 
 export function Consecutive(
     {
@@ -11,11 +16,14 @@ export function Consecutive(
         isConsecutive: boolean;
         className?: string;
     }
-) {
+): JSX.Element {
     return (
-        <Badge className={`items-center flex gap-2 ${isConsecutive ? "bg-sky-500" : "bg-rose-500"} text-white mr-5 ${className}`}>
+        <Badge className={cn(
+            'items-center flex gap-2 text-white mr-5', className,
+            isConsecutive ? "bg-sky-500 hover:bg-sky-500/90" : "bg-rose-500 hover:bg-rose-500/90"
+        )}>
             <GripHorizontal className="h-4 w-4" />
             {isConsecutive ? "Consecutivo" : "No consecutivo"}
         </Badge>
-    )
+    );
 }
