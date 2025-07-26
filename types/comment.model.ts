94e7e2507ac847da0ebe2d446f970fdb55b6e409
kevinkeyssx <keyss1700@gmail.com>
2025-07-25 21:48:14 -0400

    CreateComment & UpdateComment implemented

--- types/comment.model.ts
+++ types/comment.model.ts
@@ -1,30 +1,31 @@
 export interface StaffComment {
-    id      : string;
     name    : string;
     email   : string;
-}
-
-
-export interface RequestComment {
-    id      : string
-    name    : string;
-}
-
-
-export interface RequestDetailComment {
-    id: string;
 }
 
 
 export interface Comment {
     id              : string;
     content         : string;
-    parentCommentId : string | null;
-    request         : RequestComment | null;
-    requestDetail   : RequestDetailComment | null;
+    // request         : RequestComment | null;
+    // requestDetail   : RequestDetailComment | null;
     staff           : StaffComment | null;
     adminName       : string | null;
     adminEmail      : string | null;
     createdAt       : Date | string;
     updatedAt       : Date | string;
+}
+
+
+export interface CreateComment {
+    content             : string;
+    requestId?          : string | null | undefined;
+    requestDetailId?    : string | null | undefined;
+    adminName           : string;
+    adminEmail          : string;
+}
+
+export interface UpdateComment {
+    id      : string;
+    content : string;
 }
