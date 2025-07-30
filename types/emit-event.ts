902957bbc0e909d1e8d3099128786553f0e15a98
kevinkeyssx <keyss1700@gmail.com>
2025-07-29 22:38:05 -0400

    EmitEvent added

---
+++ types/emit-event.ts
@@ -0,0 +1,18 @@
+export enum EnumAction {
+    CREATE = 'create',
+    UPDATE = 'update',
+    DELETE = 'delete',
+}
+
+
+export enum Type {
+    REQUEST = 'request',
+    DETAIL  = 'detail'
+}
+
+
+export interface EmitEvent<T = any> {
+    message : T;
+    action  : EnumAction;
+    type    : Type;
+}
