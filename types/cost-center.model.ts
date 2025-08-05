1d42a021f6f330e2cbde5ef66007718dd424e668
kevinkeyssx <keyss1700@gmail.com>
2025-08-04 21:48:20 -0400

    CostCenter model added

---
+++ types/cost-center.model.ts
@@ -0,0 +1,9 @@
+export interface CostCenter {
+    centro_costo        : string;
+    NOMCGA              : string;
+    ARACGS              : string;
+    SARCGS              : string;
+    sub_area            : string;
+    estado              : string;
+    NombreCompuesto     : string;
+}
