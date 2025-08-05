671c4ffc4bfb64b4ea4625883813e79a95be47cb
kevinkeyssx <keyss1700@gmail.com>
2025-08-04 21:48:05 -0400

    useSpace added

---
+++ hooks/use-space.ts
@@ -0,0 +1,73 @@
+import { useMemo } from "react";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { Option }           from "@/components/shared/Combobox";
+import { KEY_QUERYS }       from "@/consts/key-queries";
+import { fetchApi }         from "@/services/fetch";
+import { Space }            from "@/types/space.model";
+import { mockFetchSpaces }  from "@/data/space.data";
+
+
+// Function to transform cost center data to options format
+export const memoizedSpaceData = (
+	spaceData : Space[] | undefined
+): Option[] => useMemo(() => {
+	return spaceData?.map( space => ({
+		id		: space.lov_vals[0].id,
+		label	: space.lov_vals[0].description,
+		value	: space.lov_vals[0].id,
+	})) ?? [];
+}, [spaceData]);
+
+
+// Interface for hook parameters
+interface UseSpaceParams {
+	enabled? : boolean;
+    useMockData?: boolean;
+}
+
+
+// Interface for hook return values
+interface UseSpaceReturn {
+	spaces      : Option[];
+	isLoading	: boolean;
+	isError		: boolean;
+	error		: Error | null;
+}
+
+
+/**
+ * Custom hook to fetch and manage cost center data
+ * @param params - Configuration parameters for the hook
+ * @returns Transformed cost center data, loading state, and error information
+ */
+export const useSpace = ( 
+	params : UseSpaceParams = {} 
+): UseSpaceReturn => {
+	const { enabled = true, useMockData = true } = params;
+
+	const {
+		data: spaces = [],
+		isLoading,
+		isError,
+		error
+	} = useQuery<Space[]>({
+		queryKey	: [KEY_QUERYS.SPACES],
+		// queryFn		: () => fetchApi({ isApi: false, url: `cost-centers/all` }),
+        queryFn: useMockData 
+            ? mockFetchSpaces
+            : () => fetchApi({ isApi: false, url: `cost-centers/all` }),
+		enabled,
+	});
+
+	// Transform the data using memoized function
+	const spaceOptions = memoizedSpaceData( spaces );
+
+	return {
+		spaces: spaceOptions,
+		isLoading,
+		isError,
+		error,
+	};
+};
