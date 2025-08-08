a59ea587e932972e69508c86f6accb38ceb53fa6
kevinkeyssx <keyss1700@gmail.com>
2025-08-08 19:47:34 -0400

    Image Logo added

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState }  from "react";
 import { useRouter }            from "next/navigation";
+import Image                    from "next/image"
 
 import {
     Menubar,
@@ -39,19 +40,35 @@ export default function Header() {
 
     return (
         <>
-            <header className="bg-black py-4 border-b border-gray-200 dark:border-gray-800 transition-colors">
-                <div className="flex justify-between items-center container mx-auto gap-2 px-4">
-                    <h1 className="text-xl xl:text-2xl font-bold text-white">Facultades académicas</h1>
+            <header className="bg-black py-2 border-b border-gray-200 dark:border-gray-800 transition-colors">
+                <div className="flex justify-between items-center container mx-auto gap-2 px-5">
+
+                    <div className="flex items-center gap-3">
+                        <a href="#">
+                            <span className="sr-only">Universidad Adolfo Ibáñez</span>
+
+                            <Image
+                                className="p-0"
+                                title       = "UAI"
+                                src         = "https://mailing20s.s3.amazonaws.com/templtates/logosinescudo.png"
+                                alt         = "logo uai"
+                                width       = {137}
+                                height      = {50}
+                            />
+                        </a>
+
+                        <h1 className="hidden sm:flex text-2xl sm:text-xl lg:text-2xl xl:text-3xl font-bold text-white">Facultades Académicas</h1>
+                    </div>
 
                     <div className="flex items-center gap-2">
                         {session.session && (<>
-                            <Menubar className="hidden md:flex bg-black text-white border-zinc-700">
+                            <Menubar className="hidden lg:flex bg-black text-white border-zinc-700">
                                 <MenubarMenu>
                                     <MenubarTrigger
                                         onClick = {() => router.push( '/faculties' )}
                                         id      = "faculty"
                                     >
-                                        <span className="hidden xl:block">Facultades</span>
+                                        Facultades
                                     </MenubarTrigger>
                                 </MenubarMenu>
 
@@ -60,7 +77,7 @@ export default function Header() {
                                         onClick = {() => router.push( '/professors' )}
                                         id      = "professor"
                                     >
-                                        <span className="hidden xl:block">Profesores</span>
+                                        Profesores
                                     </MenubarTrigger>
                                 </MenubarMenu>
                             </Menubar>
