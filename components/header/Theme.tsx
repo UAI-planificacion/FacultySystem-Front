255f12a5938e730f78cdc07b5d41f3ac4b56b7e2
kevinkeyssx <keyss1700@gmail.com>
2025-08-08 19:47:51 -0400

    theme name validated

--- components/header/Theme.tsx
+++ components/header/Theme.tsx
@@ -13,23 +13,29 @@ import { Button }   from "@/components/ui/button";
 
 
 export function Theme() {
-    const { setTheme } = useTheme();
+    const { theme, setTheme } = useTheme();
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon" className="bg-black text-white border-zinc-700 hover:bg-zinc-900 hover:text-white">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Button variant="outline" size="icon" className="bg-black text-white border-zinc-700">
+                    { theme === 'light' && 
+                        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    }
 
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    { theme === 'dark' &&
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    }
 
-                    <Computer className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    { theme === 'system' &&
+                        <Computer className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    }
 
                     <span className="sr-only">Cambiar tema</span>
                 </Button>
             </DropdownMenuTrigger>
 
-            <DropdownMenuContent align="end" className="bg-black text-white border-zinc-700">
+            <DropdownMenuContent align="end">
                 <DropdownMenuItem onClick={() => setTheme("light")} className="flex items-center justify-between">
                     Claro
 
