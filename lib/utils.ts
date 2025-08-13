df8c4caba1d55fde292c544c379d2488ae0e8ccd
kevinkeyssx <keyss1700@gmail.com>
2025-08-12 20:47:48 -0400

    getLevelName removed

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,9 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue }    from 'clsx';
+import { twMerge }                  from 'tailwind-merge';
+import { format }                   from "@formkit/tempo"
 
-import { Level, SpaceType, Status } from '@/types/request';
-
-import { format } from "@formkit/tempo"
-
+import { Status }       from '@/types/request';
+import { SpaceType }    from '@/types/request-detail.model';
 
 
 export function cn(...inputs: ClassValue[]) {
@@ -44,10 +43,3 @@ export const getSpaceType = ( spaceType: SpaceType ) => ({
     [SpaceType.LAB]         : "Laboratorio",
     [SpaceType.LABPC]       : "Laboratorio de Computadoras",
 })[spaceType];
-
-
-export const getLevelName = ( level: Level ) => ({
-    [Level.FIRST_GRADE]     : "Primer Grado",
-    [Level.SECOND_GRADE]    : "Segundo Grado",
-    [Level.PREGRADO]        : "Pregrado",
-})[level];
