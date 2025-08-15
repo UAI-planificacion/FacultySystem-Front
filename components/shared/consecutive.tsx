8b49addcdfdea7889b14c9bd0e1bcb45293050f8
kevinkeyssx <keyss1700@gmail.com>
2025-08-14 21:35:54 -0400

    Consecutive styles updated

--- components/shared/consecutive.tsx
+++ components/shared/consecutive.tsx
@@ -19,12 +19,12 @@ export function Consecutive(
 ): JSX.Element {
     return (
         <Badge className={cn(
-            'items-center flex gap-2 text-white mr-2', className,
+            'items-center inline-flex gap-1.5 text-white text-xs px-2 py-0.5 w-fit', className,
             isConsecutive ? "bg-sky-500 hover:bg-sky-500/90" : "bg-rose-500 hover:bg-rose-500/90"
         )}>
-            <GripHorizontal className="h-4 w-4" />
+            <GripHorizontal className="h-3 w-3" />
 
-            <span className="inline-flex whitespace-nowrap">
+            <span className="whitespace-nowrap">
                 {isConsecutive ? "Consecutivo" : "No consecutivo"}
             </span>
         </Badge>
