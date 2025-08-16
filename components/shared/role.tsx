b343fa153779ed9eb207b7190d8eca2a115f6106
kevinkeyssx <keyss1700@gmail.com>
2025-08-16 18:13:12 -0400

    ADMIN_FACULTY implemented

--- components/shared/role.tsx
+++ components/shared/role.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { Badge } from "@/components/ui/badge";
+import { Eye, Shield, SquarePen, UsersRound } from "lucide-react";
 
-import { Role } from "@/types/staff.model";
-import { Eye, Shield, SquarePen } from "lucide-react";
+import { Badge }    from "@/components/ui/badge";
+import { Role }     from "@/types/staff.model";
+
 
 interface RoleProps {
     role: Role;
@@ -11,23 +12,26 @@ interface RoleProps {
 
 
 const getColorRole = ( role: Role ): string => ({
-    ADMIN   : 'bg-blue-500 text-white',
-    EDITOR  : 'bg-amber-500 text-white',
-    VIEWER  : 'bg-gray-500 text-white',
+    ADMIN       : 'bg-blue-500 text-white',
+    EDITOR      : 'bg-amber-500 text-white',
+    VIEWER      : 'bg-gray-500 text-white',
+    ADMIN_FACULTY: 'bg-green-500 text-white',
 })[role] || 'bg-gray-500 text-white'
 
 
 const getNameRole = ( role: Role ): string => ({
-    ADMIN   : 'Administrador',
-    EDITOR  : 'Editor',
-    VIEWER  : 'Visualizador',
-})[role] || 'Visualizador'
+    ADMIN       : 'Administrador',
+    EDITOR      : 'Editor',
+    VIEWER      : 'Visualizador',
+    ADMIN_FACULTY: 'Admin Facultad',
+})[role] || 'Administrador de Facultad'
 
 
 const getIconRole = ( role: Role ) => ({
-    ADMIN   : <Shield className="h-4 w-4 mr-1" />,
-    EDITOR  : <SquarePen className="h-4 w-4 mr-1" />,
-    VIEWER  : <Eye className="h-4 w-4 mr-1" />,
+    ADMIN       : <Shield className="h-4 w-4 mr-1" />,
+    EDITOR      : <SquarePen className="h-4 w-4 mr-1" />,
+    VIEWER      : <Eye className="h-4 w-4 mr-1" />,
+    ADMIN_FACULTY: <UsersRound className="h-4 w-4 mr-1" />,
 })[role] || <Eye className="h-4 w-4 mr-1" />
 
 
