6e8657d13241ad6c05f775a9ee908ad913a40fd3
kevinkeyssx <keyss1700@gmail.com>
2025-08-15 21:51:50 -0400

    data & Grados routes implemented

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -25,8 +25,8 @@ import { useSession }   from "@/hooks/use-session";
 
 
 export function Login(): JSX.Element {
-    const router                    = useRouter();
-    const { session, isLoading }    = useSession();
+    const router                = useRouter();
+    const { data, isLoading }   = useSession();
 
     return (
         <>
@@ -40,7 +40,7 @@ export function Login(): JSX.Element {
                     <LoaderMini />
                 </Button>
             ) : (
-                session?.user ? (
+                data?.user ? (
                     <DropdownMenu>
                         <DropdownMenuTrigger asChild>
                             <Button
@@ -48,15 +48,15 @@ export function Login(): JSX.Element {
                                 className   = "px-1 md:px-3 gap-2 bg-black text-white border-zinc-700"
                             >
                                 <Image
-                                    src         = { session?.user?.image }
-                                    alt         = { session?.user?.name }
+                                    src         = { data.user?.image || '' }
+                                    alt         = { data.user?.name }
                                     width       = { 30 }
                                     height      = { 30 }
                                     loading     = "lazy"
                                     className   = "rounded-full"
                                 />
 
-                                <span className="hidden md:flex">{session?.user?.name}</span>
+                                <span className="hidden md:flex">{ data.user?.name }</span>
                             </Button>
                         </DropdownMenuTrigger>
 
@@ -90,12 +90,26 @@ export function Login(): JSX.Element {
 
                             <DropdownMenuSeparator className="lg:hidden" />
 
+                            <DropdownMenuItem className="p-0">
+                                <Button
+                                    size        = "sm"
+                                    type        = "button"
+                                    variant     = "ghost"
+                                    onClick     = {() => router.push( '/grades' )}
+                                    className   = "w-full flex justify-start lg:hidden"
+                                >
+                                    Grados
+                                </Button>
+                            </DropdownMenuItem>
+
+                            <DropdownMenuSeparator className="lg:hidden" />
+
                             {/* Profile */}
                             <DropdownMenuLabel>Mi cuenta</DropdownMenuLabel>
                             <DropdownMenuSeparator />
-                            <DropdownMenuLabel>{session?.user?.name}</DropdownMenuLabel>
+                            <DropdownMenuLabel>{data.user?.name}</DropdownMenuLabel>
                             <DropdownMenuSeparator />
-                            <DropdownMenuLabel>{session?.user?.email}</DropdownMenuLabel>
+                            <DropdownMenuLabel>{data.user?.email}</DropdownMenuLabel>
 
                             <DropdownMenuSeparator />
 
