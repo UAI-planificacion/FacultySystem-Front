eb30419b59be612d81075eea1ed48d65ddb7b7be
kevinkeyssx <keyss1700@gmail.com>
2025-08-22 21:33:20 -0400

    Table scroll horizontal formmat updated

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -2,18 +2,33 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-const Table = React.forwardRef<
-  HTMLTableElement,
-  React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
-    <table
-      ref={ref}
-      className={cn('w-full caption-bottom text-sm', className)}
-      {...props}
-    />
-  </div>
-));
+interface TableProps extends React.HTMLAttributes<HTMLTableElement> {
+	noWrapper? : boolean;
+}
+
+const Table = React.forwardRef<HTMLTableElement, TableProps>(({ 
+	className, 
+	noWrapper = false, 
+	...props 
+}, ref) => {
+	const tableElement = (
+		<table
+			ref={ref}
+			className={cn('w-full caption-bottom text-sm', className)}
+			{...props}
+		/>
+	);
+
+	if (noWrapper) {
+		return tableElement;
+	}
+
+	return (
+		<div className="relative w-full overflow-auto">
+			{tableElement}
+		</div>
+	);
+});
 Table.displayName = 'Table';
 
 const TableHeader = React.forwardRef<
