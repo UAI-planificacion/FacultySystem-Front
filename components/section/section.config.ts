268cf282a5075e33bde0b767c63e3beba35948da
kevinkeyssx <keyss1700@gmail.com>
2025-09-02 22:12:51 -0400

    Section Config updated

--- components/section/section.config.ts
+++ components/section/section.config.ts
@@ -1,4 +1,4 @@
-import { Session } from "@/types/section.model";
+import { SectionData, Session } from "@/types/section.model";
 
 export const sessionLabels = {
     [Session.C]: 'Cátedra',
@@ -13,3 +13,32 @@ export const sessionColors = {
     [Session.T]: 'bg-orange-500',
     [Session.L]: 'bg-purple-500',
 };
+
+
+export const sessionBorders = {
+    [Session.C]: 'border border-blue-500/70 dark:border-blue-500/30',
+    [Session.A]: 'border border-green-500/70 dark:border-green-500/30',
+    [Session.T]: 'border border-orange-500/70 dark:border-orange-500/30',
+    [Session.L]: 'border border-purple-500/70 dark:border-purple-500/30',
+};
+
+
+export interface PeriodsSection {
+    id      : string;
+    label   : string;
+    value   : string;
+}
+
+
+export interface Props {
+    section                         : SectionData | SectionData[];
+    updateSectionNumber             : ( sectionId: string, newNumber: number ) => void;
+    removeSection                   : ( sectionId: string ) => void;
+    removeDisabled                  : boolean;
+    isErrorPeriods                  : boolean;
+    updateSectionPeriod             : ( sectionId: string, period: string ) => void;
+    getAvailablePeriodsForSection   : ( currentSectionId: string ) => PeriodsSection[];
+    isLoadingPeriods                : boolean;
+    updateSessionCount              : ( sectionId: string, session: Session, delta: number ) => void
+    setSessionCount                 : ( sectionId: string, session: Session, value: string ) => void
+}
