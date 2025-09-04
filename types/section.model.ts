5c114b74cd3eeef0c7b330bed2b64aef2a281d66
kevinkeyssx <keyss1700@gmail.com>
2025-09-03 21:00:43 -0400

    isClosed implemented

--- types/section.model.ts
+++ types/section.model.ts
@@ -2,22 +2,23 @@ import { Size } from "@/types/request-detail.model";
 
 
 export interface Section {
-    id                      : string,
-    code                    : number,
-    session                 : string,
-    size                    : Size,
-    correctedRegistrants    : number,
-    realRegistrants         : number,
-    plannedBuilding         : string,
-    chairsAvailable         : number,
-    room                    : string,
-    professorName           : string,
-    professorId             : string,
-    day                     : number,
-    moduleId                : string,
-    subjectName             : string,
-    subjectId               : string,
-    period                  : string
+    id                      : string;
+    code                    : number;
+    session                 : Session;
+    size                    : Size;
+    correctedRegistrants    : number;
+    realRegistrants         : number;
+    plannedBuilding         : string;
+    chairsAvailable         : number;
+    room                    : string;
+    professorName           : string;
+    professorId             : string;
+    day                     : number;
+    moduleId                : string;
+    subjectName             : string;
+    subjectId               : string;
+    period                  : string;
+    isClosed                : boolean;
 }
 
 
