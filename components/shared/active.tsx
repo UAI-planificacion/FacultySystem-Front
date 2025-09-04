022814d4bd1949af607fc5ffc7962b15e69545f5
kevinkeyssx <keyss1700@gmail.com>
2025-09-03 21:00:34 -0400

    ActiveBadge implemented

--- components/shared/active.tsx
+++ components/shared/active.tsx
@@ -4,23 +4,31 @@ import { CircleCheckBig, CircleX } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge"
 
-export function ActiveBadge(
-    {
-        isActive
-    }: {
-        isActive: boolean;
-    }
-) {
+interface Props {
+    isActive        : boolean;
+    activeText?     : string;
+    inactiveText?   : string
+}
+
+
+export function ActiveBadge({
+    isActive,
+    activeText      = 'Activo',
+    inactiveText    = 'Inactivo'
+}: Props ) {
     return (
         <div className="flex items-center gap-2">
-            {isActive ? <CircleCheckBig className="w-4 h-4 text-green-600" /> : <CircleX className="w-4 h-4 text-red-600" />}
+            { isActive
+                ? <CircleCheckBig className="w-4 h-4 text-green-600" />
+                : <CircleX className="w-4 h-4 text-red-600" />
+            }
 
             <Badge
-                variant={isActive ? 'default' : 'destructive'}
-                className={`${isActive ? 'bg-green-600' : 'bg-red-600'} text-white`}
+                variant     = { isActive ? 'default' : 'destructive' }
+                className   = { `${isActive ? 'bg-green-600' : 'bg-red-600'} text-white` }
             >
-                {isActive ? 'Activo' : 'Inactivo'}
+                { isActive ? activeText : inactiveText }
             </Badge>
         </div>
-    )
+    );
 }
