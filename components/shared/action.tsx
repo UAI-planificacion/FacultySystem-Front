ef5fa8f22137cf2415da6ee48154455374b15b18
kevinkeyssx <keyss1700@gmail.com>
2025-09-06 23:40:30 -0400

    isDisabledDelete & isDisabledEdit implemented

--- components/shared/action.tsx
+++ components/shared/action.tsx
@@ -8,33 +8,39 @@ import { Button } from "@/components/ui/button";
 
 
 interface Props {
-    editItem    : ( obj: any ) => void;
-    deleteItem  : ( obj: any ) => void;
-    item        : any;
+    editItem            : ( obj: any ) => void;
+    deleteItem          : ( obj: any ) => void;
+    item                : any;
+    isDisabledEdit?      : boolean;
+    isDisabledDelete?    : boolean;
 }
 
 
 export function ActionButton({
     editItem,
     deleteItem,
-    item
+    item,
+    isDisabledEdit = false,
+    isDisabledDelete = false,
 }: Props ): JSX.Element {
     return (
         <div className="flex justify-end gap-1.5">
             <Button
-                title   = "Editar"
-                variant = "outline"
-                size    = "icon"
-                onClick = {() => editItem( item )}
+                title       = "Editar"
+                variant     = "outline"
+                size        = "icon"
+                disabled    = { isDisabledEdit }
+                onClick     = {() => editItem( item )}
             >
                 <Pencil className="h-4 w-4 text-blue-500" />
             </Button>
 
             <Button
-                title   = "Eliminar"
-                variant = "outline"
-                size    = "icon"
-                onClick = {() => deleteItem( item )}
+                title       = "Eliminar"
+                variant     = "outline"
+                size        = "icon"
+                disabled    = { isDisabledDelete }
+                onClick     = {() => deleteItem( item )}
             >
                 <Trash2 className="h-4 w-4 text-red-500" />
             </Button>
