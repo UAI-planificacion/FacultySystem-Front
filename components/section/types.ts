c19f34adbae9fba9961500732eaf32f1e0e33ab9
kevinkeyssx <keyss1700@gmail.com>
2025-09-06 23:40:15 -0400

    Sections types for components added

---
+++ components/section/types.ts
@@ -0,0 +1,25 @@
+import { Section, Session } from "@/types/section.model";
+
+export interface Option {
+    id      : string;
+    label   : string;
+    value   : string;
+}
+
+export interface SessionCount {
+    [Session.C] : number;
+    [Session.A] : number;
+    [Session.T] : number;
+    [Session.L] : number;
+}
+
+
+export interface SectionGroup {
+    groupId         : string;
+    code            : number;
+    period          : string;
+    sessionCounts   : SessionCount;
+    schedule        : string;
+    isOpen          : boolean;
+    sections        : Section[];
+}
