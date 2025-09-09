74146c328c389e6c61ee4fa993780ed7e6c7d52e
kevinkeyssx <keyss1700@gmail.com>
2025-09-09 00:22:39 -0300

    SpaceSelect added

---
+++ components/shared/item-select/space-select.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { JSX } from "react";
+
+import { MultiSelectCombobox }  from "@/components/shared/Combobox";
+import { Label }                from "@/components/ui/label";
+import { Input }                from "@/components/ui/input";
+import { Props }                from "@/components/shared/item-select/select-props";
+
+import { useSpace } from "@/hooks/use-space";
+
+
+export function SpaceSelect({
+    defaultValues,
+    onSelectionChange,
+    multiple    = true,
+    label,
+    placeholder = 'Seleccionar Espacios'
+} : Props ): JSX.Element {
+    const {
+        spaces,
+        isLoading,
+        isError
+    } = useSpace({ enabled: true });
+
+
+    return (
+        <div className="space-y-2">
+            { label && <Label htmlFor="space">{ label }</Label> }
+
+            { isError ? (
+                <div className="space-y-1">
+                    <Input
+                        placeholder = "ID del Espacio"
+                        onChange    = {( event ) => onSelectionChange? onSelectionChange( event.target.value ): undefined}
+                        className   = "h-8"
+                    />
+
+                    <span className="text-xs text-muted-foreground">
+                        Error al cargar los espacios. Ingrese el ID manualmente.
+                    </span>
+                </div>
+            ) : (
+                <MultiSelectCombobox
+                    options             = { spaces }
+                    defaultValues       = { defaultValues }
+                    onSelectionChange   = { onSelectionChange }
+                    placeholder         = { placeholder }
+                    disabled            = { isLoading }
+                    multiple            = {  multiple }
+                />
+            )}
+        </div>
+    );
+}
