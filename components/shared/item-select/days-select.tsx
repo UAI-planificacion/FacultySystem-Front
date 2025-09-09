3064e6362282a333e9db7976cfa041234da7dc85
kevinkeyssx <keyss1700@gmail.com>
2025-09-09 00:21:27 -0300

    Days Selector added

---
+++ components/shared/item-select/days-select.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { JSX, useMemo } from "react";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { MultiSelectCombobox }  from "@/components/shared/Combobox";
+import { Label }                from "@/components/ui/label";
+import { Input }                from "@/components/ui/input";
+import { Props }                from "./select-props";
+
+import { KEY_QUERYS }   from "@/consts/key-queries";
+import { fetchApi }     from "@/services/fetch";
+import { ENV }          from "@/config/envs/env";
+import { Day }          from "@/types/request";
+
+
+export function DaySelect({
+    defaultValues,
+    onSelectionChange,
+    multiple    = true,
+    label,
+    placeholder = 'Seleccionar Días'
+} : Props ): JSX.Element {
+    const {
+        data,
+        isLoading,
+        isError,
+    } = useQuery({
+        queryKey    : [ KEY_QUERYS.DAYS ],
+        queryFn     : () => fetchApi<Day[]>({
+            url         : `${ENV.ACADEMIC_SECTION}days`,
+            isApi       : false
+        }),
+    });
+
+
+    const memoizeDays = useMemo(() => {
+        return data?.map( day => ({
+            id      : day.id.toString(),
+            label   : day.name,
+            value   : day.id.toString()
+        }) ) ?? [];
+    }, [data]);
+
+
+    return (
+        <div className="space-y-2">
+            { label && <Label htmlFor="day">{ label }</Label> }
+
+            { isError ? (
+                <div className="space-y-1">
+                    <Input
+                        placeholder = "ID del día"
+                        onChange    = {( event ) => onSelectionChange? onSelectionChange( event.target.value ): undefined}
+                        className   = "h-8"
+                    />
+
+                    <span className="text-xs text-muted-foreground">
+                        Error al cargar los días. Ingrese el ID manualmente.
+                    </span>
+                </div>
+            ) : (
+                <MultiSelectCombobox
+                    options             = { memoizeDays }
+                    defaultValues       = { defaultValues }
+                    onSelectionChange   = { onSelectionChange }
+                    placeholder         = { placeholder }
+                    disabled            = { isLoading }
+                    multiple            = {  multiple }
+                />
+            )}
+        </div>
+    );
+}
