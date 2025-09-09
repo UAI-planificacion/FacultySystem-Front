8790d9c33714b9335c9f66087cfe67e57b23707e
kevinkeyssx <keyss1700@gmail.com>
2025-09-09 00:22:18 -0300

    Props for items-selectors added

---
+++ components/shared/item-select/select-props.ts
@@ -0,0 +1,7 @@
+export interface Props {
+    defaultValues       : string | string[] | undefined;
+    onSelectionChange?  : ( selectedValues: string[] | string | undefined ) => void;
+    multiple?           : boolean;
+    label?              : string;
+    placeholder?        : string;
+}
