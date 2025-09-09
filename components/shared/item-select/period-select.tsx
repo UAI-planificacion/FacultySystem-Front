197a474e926fbfa59b498e03bf34769514b86022
kevinkeyssx <keyss1700@gmail.com>
2025-09-09 00:21:53 -0300

    PeriodSelect added

---
+++ components/shared/item-select/period-select.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { JSX, useMemo } from "react";
+
+import { useQuery } from "@tanstack/react-query";
+import { format }   from "@formkit/tempo";
+
+import { MultiSelectCombobox }  from "@/components/shared/Combobox";
+import { Label }                from "@/components/ui/label";
+import { Input }                from "@/components/ui/input";
+
+import { KEY_QUERYS }   from "@/consts/key-queries";
+import { fetchApi }     from "@/services/fetch";
+import { ENV }          from "@/config/envs/env";
+import { Props }        from "./select-props";
+import { Period }       from "@/types/periods.model";
+
+
+function formatDate( period : Period ): string {
+    if ( !period.startDate || !period.endDate ) return '';
+
+    return ` (${format( period.startDate, 'short' )} - ${format( period.endDate, 'short' )})`;
+}
+
+
+export function PeriodSelect({
+    defaultValues,
+    onSelectionChange,
+    multiple    = true,
+    label,
+    placeholder = 'Seleccionar Periodos'
+} : Props ): JSX.Element {
+    const {
+        data,
+        isLoading,
+        isError
+    } = useQuery<Period[]>({
+        queryKey: [KEY_QUERYS.PERIODS],
+        queryFn : () => fetchApi<Period[]>({
+            isApi   : false,
+            url     : `${ENV.ACADEMIC_SECTION}periods`
+        }),
+    });
+
+
+    const memoizePeriods = useMemo(() => {
+        return data?.map( period => ({
+            id      : period.id,
+            label   : `${period.id} - ${ period.name }${ formatDate( period )}`,
+            value   : period.id
+        }) ) ?? [];
+    }, [data]);
+
+
+    return (
+        <div className="space-y-2">
+            { label && <Label htmlFor="period">{ label }</Label> }
+
+            { isError ? (
+                <div className="space-y-1">
+                    <Input
+                        placeholder = "ID del período"
+                        onChange    = {( event ) => onSelectionChange? onSelectionChange( event.target.value ): undefined}
+                        className   = "h-8"
+                    />
+
+                    <span className="text-xs text-muted-foreground">
+                        Error al cargar los períodos. Ingrese el ID manualmente.
+                    </span>
+                </div>
+            ) : (
+                <MultiSelectCombobox
+                    options             = { memoizePeriods }
+                    defaultValues       = { defaultValues }
+                    onSelectionChange   = { onSelectionChange }
+                    placeholder         = { placeholder }
+                    disabled            = { isLoading }
+                    multiple            = {  multiple }
+                />
+            )}
+        </div>
+    );
+}
