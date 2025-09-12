8fd7b26633ad3bb470d4ef99d2033377cdf966fb
kevinkeyssx <keyss1700@gmail.com>
2025-09-11 22:38:06 -0300

    Space model updated

--- types/space.model.ts
+++ types/space.model.ts
@@ -1,21 +1,32 @@
 export type Skill = {
-    id : string;
-    floor: string;
-    min?: number;
-    max?: number
+    floor       : number | null;
+    capacity    : number | null;
+    aforo       : number | null;
+    mts2        : string | null;
+    foto        : string | null;
+    num_enchufe : number | null;
+    type        : string | null;
+    size        : string | null;
+    min?        : number | null;
+    max?        : number | null;
 }
 
 
 export type LovVal = {
-    id          : string;
+    idlovvals   : number;
     description : string;
     active      : boolean;
     skill       : Skill;
+    created_at  : string;
+    comment     : string | null
 }
 
 
 export type Space = {
-    id          : string,
+    idlov       : number,
     description : string;
-    lov_vals    : LovVal[];
+    lov_vals    : LovVal[]  | null;
+    created_at  : string    | null;
+    skill       : any       | null;
+    active      : boolean;
 }
