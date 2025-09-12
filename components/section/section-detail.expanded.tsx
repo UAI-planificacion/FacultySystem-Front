91b80cd24570a0806eeaac2d70369a46510c379f
kevinkeyssx <keyss1700@gmail.com>
2025-09-11 22:36:53 -0300

    Styles updated

--- components/section/section-detail.expanded.tsx
+++ components/section/section-detail.expanded.tsx
@@ -113,89 +113,89 @@ export function SectionDetailExpanded({
 
     return (
         <>
-        <TableRow>
-            <TableCell colSpan={9} className="p-0">
-                <div className="border-l-4">
-                    <Table>
-                        <TableHeader>
-                            <TableRow className="">
-                                <TableHead className="w-12">Seleccionar</TableHead>
-                                <TableHead className="pl-12">Sesión</TableHead>
-                                <TableHead>Tamaño</TableHead>
-                                <TableHead>Sala</TableHead>
-                                <TableHead>Profesor</TableHead>
-                                <TableHead>Día</TableHead>
-                                <TableHead>Módulo</TableHead>
-                                <TableHead className="text-right">Acciones</TableHead>
-                            </TableRow>
-                        </TableHeader>
-
-                        <TableBody>
-                            {group.sections.map(( section ) => (
-                                <TableRow key={section.id} className="border-l-4 border-transparent">
-                                    <TableCell className="w-12">
-                                        <Checkbox
-                                            checked         = { selectedSections.has( section.id ) }
-                                            onCheckedChange = {() => handleSectionSelection( section.id, group.groupId )}
-                                            aria-label      = "Seleccionar sección"
-                                            className       = "w-5 h-5"
-                                            disabled        = { !group.isOpen }
-                                        />
-                                    </TableCell>
-
-                                    <TableCell className="pl-12">
-                                        <SessionName session={ section.session } />
-                                    </TableCell>
-
-                                    <TableCell>
-                                        <Badge variant={section.size ? 'default': 'outline'}>
-                                            { section.size ?? '-' }
-                                        </Badge>
-                                    </TableCell>
-
-                                    <TableCell>{ section.room ?? '-' }</TableCell>
-
-                                    <TableCell>{ section.professor?.name ?? '-' }</TableCell>
-
-                                    <TableCell>{ days[(section.day?.id ?? -1) - 1] ?? '-' }</TableCell>
-
-                                    <TableCell title={ section.module?.code ? `M${section.module.code}${ section.module.diference ? `-${section.module.diference}` : '' } ${section.module.startHour}:${section.module.endHour}` : '' }>
-                                        { section.module?.code ? `M${section.module.code} ${ section.module.diference ? `-${section.module.diference}` : '' }` : '-' }
-                                    </TableCell>
-
-                                    <TableCell className="text-right">
-                                        <ActionButton
-                                            editItem            = {() => handleEdit( section )}
-                                            deleteItem          = {() => handleDelete( section )}
-                                            item                = { section }
-                                            isDisabledEdit      = { !group.isOpen }
-                                            isDisabledDelete    = { !group.isOpen }
-                                        />
-                                    </TableCell>
+            <TableRow>
+                <TableCell colSpan={9} className="p-0">
+                    <div className="border-l-4 ml-16">
+                        <Table>
+                            <TableHeader>
+                                <TableRow className="">
+                                    <TableHead className="w-12">Seleccionar</TableHead>
+                                    <TableHead className="pl-12">Sesión</TableHead>
+                                    <TableHead>Tamaño</TableHead>
+                                    <TableHead>Sala</TableHead>
+                                    <TableHead>Profesor</TableHead>
+                                    <TableHead>Día</TableHead>
+                                    <TableHead>Módulo</TableHead>
+                                    <TableHead className="text-right">Acciones</TableHead>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </div>
-            </TableCell>
-        </TableRow>
+                            </TableHeader>
 
-        {/* Edit Section Dialog */}
-        <SectionForm
-            isOpen  = { isEditSection }
-            onClose = { handleCloseSectionDialog }
-            section = { selectedSectionEdit }
-            onSave  = { handleSaveSection }
-        />
+                            <TableBody>
+                                {group.sections.map(( section ) => (
+                                    <TableRow key={section.id} className="border-l-4 border-transparent">
+                                        <TableCell className="w-12">
+                                            <Checkbox
+                                                checked         = { selectedSections.has( section.id ) }
+                                                onCheckedChange = {() => handleSectionSelection( section.id, group.groupId )}
+                                                aria-label      = "Seleccionar sección"
+                                                className       = "w-5 h-5"
+                                                disabled        = { !group.isOpen }
+                                            />
+                                        </TableCell>
 
-        {/* Delete Confirmation Dialog */}
-        <DeleteConfirmDialog
-            isOpen      = { isOpenAlert }
-            onClose     = { () => setIsOpenAlert( false )}
-            onConfirm   = { handleConfirmDeleteSection }
-            name        = { selectedAlert ? `${selectedAlert.code} - ${selectedAlert.session}` : '' }
-            type        = { "la Sección" }
-        />
+                                        <TableCell className="pl-12">
+                                            <SessionName session={ section.session } />
+                                        </TableCell>
+
+                                        <TableCell>
+                                            <Badge variant={section.size ? 'default': 'outline'}>
+                                                { section.size ?? '-' }
+                                            </Badge>
+                                        </TableCell>
+
+                                        <TableCell>{ section.room ?? '-' }</TableCell>
+
+                                        <TableCell>{ section.professor?.name ?? '-' }</TableCell>
+
+                                        <TableCell>{ days[(section.day?.id ?? -1) - 1] ?? '-' }</TableCell>
+
+                                        <TableCell title={ section.module?.code ? `M${section.module.code}${ section.module.diference ? `-${section.module.diference}` : '' } ${section.module.startHour}:${section.module.endHour}` : '' }>
+                                            { section.module?.code ? `M${section.module.code} ${ section.module.diference ? `-${section.module.diference}` : '' }` : '-' }
+                                        </TableCell>
+
+                                        <TableCell className="text-right">
+                                            <ActionButton
+                                                editItem            = {() => handleEdit( section )}
+                                                deleteItem          = {() => handleDelete( section )}
+                                                item                = { section }
+                                                isDisabledEdit      = { !group.isOpen }
+                                                isDisabledDelete    = { !group.isOpen }
+                                            />
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                </TableCell>
+            </TableRow>
+
+            {/* Edit Section Dialog */}
+            <SectionForm
+                isOpen  = { isEditSection }
+                onClose = { handleCloseSectionDialog }
+                section = { selectedSectionEdit }
+                onSave  = { handleSaveSection }
+            />
+
+            {/* Delete Confirmation Dialog */}
+            <DeleteConfirmDialog
+                isOpen      = { isOpenAlert }
+                onClose     = { () => setIsOpenAlert( false )}
+                onConfirm   = { handleConfirmDeleteSection }
+                name        = { selectedAlert ? `${selectedAlert.code} - ${selectedAlert.session}` : '' }
+                type        = { "la Sección" }
+            />
         </>
     );
 }
