6d209d035622ed1b4491ee8267228ad1bf499e2b
kevinkeyssx <keyss1700@gmail.com>
2025-09-15 22:20:54 -0300

    href="/plantilla_asignatura.xlsx" updated

--- components/subject/subject-upload.tsx
+++ components/subject/subject-upload.tsx
@@ -318,7 +318,7 @@ export function SubjectUpload({
             <Card>
 				<CardContent className="p-4 w-full">
 					<a
-                        href="/plantilla.xlsx"
+                        href="/plantilla_asignatura.xlsx"
                         download
                         className="border-4 hover:border-primary/50 rounded-lg cursor-pointer transition-colors border-dashed px-4 py-2 w-full grid justify-center content-center gap-1"
                     >
