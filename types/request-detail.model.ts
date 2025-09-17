6ae3a28796afbb6b2efc673c532e31f7b1c579c8
kevinkeyssx <keyss1700@gmail.com>
2025-09-16 22:17:10 -0300

    Tabs ordered

--- types/request-detail.model.ts
+++ types/request-detail.model.ts
@@ -4,7 +4,7 @@ import { StaffRequest } from "@/types/request";
 
 export enum SpaceType {
     ROOM        = "ROOM",
-    AUDITORIO  = "AUDITORIO",
+    AUDITORIO   = "AUDITORIO",
     COMMUNIC    = "COMMUNIC",
     LAB         = "LAB",
     LABPC       = "LABPC",
