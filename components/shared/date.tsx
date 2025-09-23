e351bf1cb03287b2226e55feaa4e0d8f7de8ca06
kevinkeyssx <keyss1700@gmail.com>
2025-09-22 21:40:31 -0300

    Styles & tabs updated

--- components/shared/date.tsx
+++ components/shared/date.tsx
@@ -1,26 +1,29 @@
 'use client'
 
+import { JSX } from "react";
+
 import { Calendar } from "lucide-react"
 
 import { tempoFormat } from "@/lib/utils";
 
-export interface ShowDateProps {
-    date: Date | string | undefined;
-    className?: string,
-    size? : string;
+
+export interface Props {
+    date        : Date | string | undefined;
+    className?  : string;
+    size?       : string;
 }
 
-export function ShowDate(
-    {
-        date,
-        className,
-        size = "h-4 w-4"
-    }: ShowDateProps
-) {
+
+export function ShowDate({
+    date,
+    className,
+    size = "h-4 w-4"
+}: Props ): JSX.Element {
     return (
         <div className="flex items-center gap-1.5">
             <Calendar className={size} />
-            <span className={className}>{tempoFormat( date )}</span>
+
+            <span className={className}>{ tempoFormat( date )}</span>
         </div>
-    )
+    );
 }
