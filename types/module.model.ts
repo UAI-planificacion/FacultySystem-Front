b72894ed6a63d0c3f16e108b6e941ca3d9d32444
kevinkeyssx <keyss1700@gmail.com>
2025-09-25 21:04:03 -0300

    Module model addeded

---
+++ types/module.model.ts
@@ -0,0 +1,32 @@
+export type Difference = 'A' | 'B' | 'C' | 'D' | null | undefined;
+
+
+interface ModuleBase {
+    id          : string;
+    code        : string;
+    isActive    : boolean;
+    name        : string;
+    difference? : Difference;
+    startHour   : string;
+    endHour     : string;
+}
+
+export interface Module extends ModuleBase {
+    dayId       : number;
+    order       : number;
+    dayModuleId : number;
+}
+
+export interface ModuleOriginal extends ModuleBase {
+    days        : number[];
+    createdAt   : Date;
+    updatedAt   : Date;
+}
+
+
+export interface ModuleCreate {
+    code        : string;
+    startHour   : string;
+    endHour     : string;
+    dayIds      : number[];
+}
