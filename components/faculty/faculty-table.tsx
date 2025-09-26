d3ac861d81abf88017d39e0af7406b4700cb1748
kevinkeyssx <keyss1700@gmail.com>
2025-09-26 20:54:42 -0300

    ScrollArea updated

--- components/faculty/faculty-table.tsx
+++ components/faculty/faculty-table.tsx
@@ -138,7 +138,7 @@ export function FacultyTable({
 	return (
         <Card>
             <CardContent>
-                <ScrollArea className="h-[calc(100vh-375px)]">
+                <ScrollArea className="h-[calc(100vh-400px)]">
                     <Table>
                         <TableHeader>
                             <TableRow>
