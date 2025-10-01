313d10d6c212f37fe8077292615b67f089cea238
kevinkeyssx <keyss1700@gmail.com>
2025-09-30 22:33:50 -0300

    Styles updated

--- app/faculties/page.tsx
+++ app/faculties/page.tsx
@@ -224,7 +224,7 @@ export default function FacultiesPage() {
         >
             <div className="flex flex-col h-full space-y-4 overflow-hidden">
                 {/* Statistics Cards */}
-                <div className="flex-shrink-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+                <div className="flex-shrink-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                     <StatisticCard
                         title   = "Total de Facultades"
                         value   = { data?.faculties.length }
