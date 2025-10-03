26276727c1e8367d3abc512d0b3ed2f55cf359d5
kevinkeyssx <keyss1700@gmail.com>
2025-10-03 20:38:45 -0300

    SessionBase added

---
+++ types/session-request.model.ts
@@ -0,0 +1,31 @@
+import { Session } from './section.model';
+
+
+export interface SessionBase {
+	spaceId?                : string | null;
+	isEnglish?              : boolean | null;
+	chairsAvailable?        : number | null;
+	correctedRegistrants?   : number | null;
+	realRegistrants?        : number | null;
+	plannedBuilding?        : string | null;
+	professorId?            : string | null;
+	dayModuleId?            : number | null;
+	date?                   : Date | null;
+}
+
+
+export interface CreateSessionRequest extends SessionBase {
+	name        : Session;
+	offerId     : string;
+}
+
+
+export interface UpdateSessionRequest extends SessionBase {
+	id          : string;
+	name?       : Session | null;
+}
+
+
+export interface UpdateMassiveSessionRequest extends SessionBase {
+	name?       : Session | null;
+}
