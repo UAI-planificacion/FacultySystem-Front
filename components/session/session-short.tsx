f029fa4c76f98cd3db2f7c34e0ff4ba42160fc60
kevinkeyssx <keyss1700@gmail.com>
2025-10-04 21:38:43 -0300

    SessionShort moved to sessions

---
+++ components/session/session-short.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { JSX } from "react";
+
+import { Session }      from "@/types/section.model";
+import { Offer }        from "@/types/offer.model";
+import { SessionName }  from "@/components/session/session-name";
+import { SessionCount } from "@/components/section/types";
+
+
+interface Props {
+    sessionCounts   : SessionCount;
+    showZero?       : boolean
+}
+
+
+export const getSessionCounts = ( offer : Offer ): SessionCount => ({
+    C: offer.lecture,
+    A: offer.tutoringSession,
+    T: offer.workshop,
+    L: offer.laboratory,
+});
+
+
+export function SessionShort({
+    sessionCounts,
+    showZero = false
+}: Props ): JSX.Element {
+    return (
+        <div className="flex gap-1 items-center">
+            { Object.entries( sessionCounts ).map( ( [ session, count ] ) => {
+                if ( showZero || count > 0 ) {
+                    return (
+                        <SessionName
+                            key     = { session }
+                            session = { session as Session }
+                            isShort = { true }
+                            count   = { count }
+                        />
+                    );
+                }
+                return null;
+            }) }
+        </div>
+    );
+}
