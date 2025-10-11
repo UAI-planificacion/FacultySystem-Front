907716c59abf80afc2778bfac2bdf99716d36e77
kevinkeyssx <keyss1700@gmail.com>
2025-10-10 21:59:12 -0300

    RequestSessionCreate & RequestSession updated

--- types/request-session.model.ts
+++ types/request-session.model.ts
@@ -1,29 +1,23 @@
-import { OfferSectionProffesor } from "./offer-section.model";
-import { StaffRequest } from "./request";
-import { Size, SpaceType } from "./request-detail.model";
-import { Session } from "./section.model";
+import { OfferSectionProffesor }    from "@/types/offer-section.model";
+import { StaffRequest }             from "@/types/request";
+import { Size, SpaceType }          from "@/types/request-detail.model";
+import { Session }                  from "@/types/section.model";
+
 
 export interface RequestSession {
     id              : string;
-    session           : Session;
+    session         : Session;
     description     : string | null;
     isEnglish       : boolean;
-    isConsecutive       : boolean;
-    inAfternoon       : boolean;
-    spaceSize : Size;
-    spaceType : SpaceType;
+    isConsecutive   : boolean;
+    inAfternoon     : boolean;
+    spaceSize       : Size;
+    spaceType       : SpaceType;
     professor       : OfferSectionProffesor;
     createdAt       : Date;
     updatedAt       : Date;
-
     staffCreate     : StaffRequest;
     staffUpdate     : StaffRequest | null;
-
-
-    // totalDetails    : number;
-    // facultyId       : string;
-
-    // section        : RequestSection
 }
 
 
@@ -33,16 +27,10 @@ export interface RequestSessionCreate {
     isEnglish       : boolean;
     isConsecutive   : boolean;
     inAfternoon     : boolean;
-    spaceSizeId       : string | null;
+    spaceSizeId     : string | null;
     spaceType       : SpaceType | null;
     professorId     : string | null;
-    staffCreate     : StaffRequest;
     spaceId         : string | null;
-
+    building        : string;
     dayModulesId    : number[];
-
-    // totalDetails    : number;
-    // facultyId       : string;
-
-    // section        : RequestSection
 }
