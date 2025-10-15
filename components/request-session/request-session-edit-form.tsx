a8d6a1909256030378225174ba16e9b077ab0399
kevinkeyssx <keyss1700@gmail.com>
2025-10-14 21:31:26 -0300

    RequestSessionEditForm styles updated

--- components/request-session/request-session-edit-form.tsx
+++ components/request-session/request-session-edit-form.tsx
@@ -210,7 +210,7 @@ export function RequestSessionEditForm({
 
 	return (
 		<Dialog open={ isOpen } onOpenChange={ onClose }>
-			<DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto">
+			<DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto">
 				<DialogHeader>
 					<DialogTitle>
 						Editar Sesión - { requestSession ? sessionLabels[requestSession.session] : '' }
