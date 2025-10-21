fdb4ae9ee4fdcddb3874e71aa7aacba977c746c4
kevinkeyssx <keyss1700@gmail.com>
2025-10-20 21:21:49 -0300

    PlanningChange updated

--- types/planning-change.model.ts
+++ types/planning-change.model.ts
@@ -1,32 +1,48 @@
-import { OfferSection, OfferSectionProffesor } from '@/types/offer-section.model';
-import { Status } from "./request";
-import { BuildingEnum, Size, SpaceType } from "./request-detail.model";
-import { Session } from "./section.model";
-import { SizeId } from "./size.model";
-import { SessionBase } from './session-request.model';
+import {
+    BuildingEnum,
+    Size,
+    SpaceType
+}                                   from "@/types/request-detail.model";
+import { OfferSectionProffesor }    from '@/types/offer-section.model';
+import { Status }                   from "@/types/request";
+import { Session }                  from "@/types/section.model";
+import { SizeId }                   from "@/types/size.model";
+import { Staff }                    from "@/types/staff.model";
+
+
+interface StaffPlanningChange extends Staff {
+    id          : string;
+    name : string
+}
 
 
 export interface PlanningChange {
     id              : string;
-    sessionName     : Session;
-    professor       : OfferSectionProffesor;
-    spaceSize       : Size          | null;
-    spaceType       : SpaceType     | null;
-    spaceId         : string        | null;
-    isEnglish       : boolean;
-    isConsecutive   : boolean;
-    inAfternoon     : boolean;
-    description     : string        | null;
-    building        : BuildingEnum  | null;
     title           : string;
     status          : Status;
-    sessionId       : SessionBase;
+    sessionName     : Session               | null;
+    building        : BuildingEnum          | null;
+    spaceId         : string                | null;
+    isEnglish       : boolean;
+    isConsecutive   : boolean;
+    description     : string                | null;
+    spaceType       : SpaceType             | null;
+    inAfternoon     : boolean;
+    professor       : OfferSectionProffesor | null;
+    spaceSize       : Size                  | null;
+    sessionId       : string                | null;
+    sectionId       : string                | null;
+    createdAt       : Date;
+    updatedAt       : Date;
+    staffCreate     : StaffPlanningChange;
+    staffUpdate     : StaffPlanningChange   | null;
     dayModulesId    : number[];
 }
 
 
 interface PlanningChangeSave {
-    sessionName     : Session | null;
+    title           : string;
+    sessionName     : Session       | null;
     professorId     : string        | null;
     spaceSizeId     : SizeId        | null;
     spaceType       : SpaceType     | null;
@@ -36,28 +52,27 @@ interface PlanningChangeSave {
     inAfternoon     : boolean       | null;
     description     : string        | null;
     building        : BuildingEnum  | null;
-    title           : string;
     status          : Status        | null;
-    sessionId       : string        | null;
+    // sessionId       : string        | null;
     dayModulesId    : number[];
 }
 
 
 export interface PlanningChangeCreate extends PlanningChangeSave {
     staffCreateId   : string;
+    sessionId       : string        | null;
     sectionId       : string | null;
 }
 
 
 export interface PlanningChangeUpdate extends PlanningChangeSave {
-    id              : string;
-    staffUpdateId   : string;
+    staffUpdateId: string;
 }
 
 
 interface SubjectSectionPlanningChange {
-    id : string;
-    name : string;
+    id      : string;
+    name    : string;
 }
 
 
@@ -69,19 +84,22 @@ interface SectionSeccionPlanningChange {
     subject     : SubjectSectionPlanningChange
 }
 
+
 interface ModulePlanningChange {
-    id : number;
-    startHour : string;
-    endHour : string;
-    difference : string;
-    code : string;
+    id          : number;
+    startHour   : string;
+    endHour     : string;
+    difference  : string;
+    code        : string;
 }
+
 
 interface DayModuleSessionPlanningChange {
     id      : number;
     dayId   : number;
     module  : ModulePlanningChange;
 }
+
 
 export interface SessionWithoutPlanningChange {
     id          : string;
