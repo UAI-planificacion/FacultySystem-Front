a2dd21600180db27cf08391d691fa8dd6d5e7ef6
kevinkeyssx <keyss1700@gmail.com>
2025-10-22 21:33:56 -0300

    Props updated

--- hooks/use-available-dates.ts
+++ hooks/use-available-dates.ts
@@ -1,33 +1,46 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { fetchApi }     from '@/services/fetch';
-import { KEY_QUERYS }   from '@/consts/key-queries';
+import { Method, fetchApi } from '@/services/fetch';
+import { KEY_QUERYS }       from '@/consts/key-queries';
 
 
-interface UseAvailableDatesParams {
-	sectionId       : string | null;
+interface Props {
+	sessionId       : string | null;
 	dayModuleId     : number | null;
-	spaceId         : string | null | undefined;
+	spaceId         : string | null;
 	enabled?        : boolean;
+    professorId?    : string | null;
 }
-
 
 /**
  * Hook to fetch available dates for a specific session, day-module, and space combination
  */
-export function useAvailableDates({ sectionId, dayModuleId, spaceId, enabled = true }: UseAvailableDatesParams ) {
+export function useAvailableDates({
+    sessionId,
+    dayModuleId,
+    spaceId,
+    professorId,
+    enabled = true
+}: Props ) {
 	return useQuery({
-		queryKey    : [ KEY_QUERYS.AVAILABLE_DATES, sectionId, dayModuleId, spaceId ],
+		queryKey    : [ KEY_QUERYS.AVAILABLE_DATES, sessionId, dayModuleId, spaceId, professorId ],
 		queryFn     : async () => {
 			const dates = await fetchApi<string[]>({
-				url : `sessions/availables/${sectionId}/${dayModuleId}/${spaceId}`
+                method : Method.POST,
+                url : `sessions/availables`,
+                body : {
+                    sessionId,
+                    dayModuleId,
+                    spaceId,
+                    professorId
+                }
 			});
 
             if ( !dates ) return [];
 
 			return dates.map( dateStr => new Date( dateStr ));
 		},
-		enabled     : enabled && !!sectionId && !!dayModuleId && !!spaceId,
+		enabled     : enabled && !!sessionId && !!dayModuleId && !!spaceId,
 		staleTime   : 1000 * 60 * 5, // 5 minutes
 	});
 }
