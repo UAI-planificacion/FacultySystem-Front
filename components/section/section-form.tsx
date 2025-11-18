61bcb179d5973221686dc21416f0d21cd7ccab9c
kevinkeyssx <keyss1700@gmail.com>
2025-11-17 23:05:24 -0300

    spaceIds map updated

--- components/section/section-form.tsx
+++ components/section/section-form.tsx
@@ -278,7 +278,8 @@ export function SectionForm({
                             </div>
                         </div>
 
-                        {( section?.sessions.map( s => s.spaceId ).length ?? 0 ) === 0 &&
+                        {/* {( section?.sessions.map( s => s.spaceId ).length ?? 0 ) === 0 && */}
+                        { section?.sessions.spaceIds.length === 0 &&
                             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                                 {/* Start Date Field */}
                                 <FormField
