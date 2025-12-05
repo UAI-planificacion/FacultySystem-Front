1fdeea6637f2e30c35b4f707a6e0274e37983335
kevinkeyssx <keyss1700@gmail.com>
2025-12-04 22:59:22 -0300

    Tabs implemented

--- types/size.model.ts
+++ types/size.model.ts
@@ -1,21 +1,24 @@
 export type SizeId = "XS" | "XE" | "S" | "SE" | "MS" | "M" | "L" | "XL" | "XXL";
 
+
 export interface Size {
-	id          : SizeId;
-	detail      : string;
-	min?        : number;
-	max?        : number;
-	greaterThan?: number;
-	lessThan?   : number;
+	id              : SizeId;
+	detail          : string;
+	min?            : number;
+	max?            : number;
+	greaterThan?    : number;
+	lessThan?       : number;
 }
 
+
 export interface SizeSave {
-	id          : SizeId;
-	detail      : string;
-	min?        : number;
-	max?        : number;
-	greaterThan?: number;
-	lessThan?   : number;
+	id              : SizeId;
+	detail          : string;
+	min?            : number;
+	max?            : number;
+	greaterThan?    : number;
+	lessThan?       : number;
 }
+
 
 export type Sizes = Size;
