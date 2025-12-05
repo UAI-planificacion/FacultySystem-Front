868251e69dbafca2691f6938f2d9d3191bc11a87
kevinkeyssx <keyss1700@gmail.com>
2025-12-04 22:58:46 -0300

    The panel width in the mobile view has been updated

--- components/shared/panel.tsx
+++ components/shared/panel.tsx
@@ -61,7 +61,7 @@ export function Panel({
             <div
                 ref         = { panelRef }
                 className   = { cn(
-                    "fixed top-1/2 -translate-y-1/2 right-0 max-h-[calc(100vh-100px)] w-full md:w-80 z-50 transition-transform duration-300 ease-in-out",
+                    "fixed top-1/2 -translate-y-1/2 right-0 max-h-[calc(100vh-100px)] w-[95%] ml-4 md:ml-0 md:w-80 z-50 transition-transform duration-300 ease-in-out",
                     isPanelOpen ? 'translate-x-0' : 'translate-x-full'
                 )}
             >
